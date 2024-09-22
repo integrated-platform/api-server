418c564ecb10a8ab590039adcad0674d952c4b4c
daev681 <wow1186@naver.com>
2024-09-22 15:24:34 +0900

    init : 프로젝트 세팅

---
+++ src/test/java/com/huntstore/api_server/ApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huntstore.api_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
